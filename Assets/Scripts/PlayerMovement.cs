74601d6ba00fa58a52cbd4fa2b94510ae09d7ffa
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2023-09-10 18:02:52 +0800

    -Updated Character movement to use Rigidbody than using CharacterController component

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
     public Transform cam;
 
     public float speed = 6f;
-
+    public Rigidbody rb;
     public float turnSmoothTime = 0.1f;
     private float turnSmoothVel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -32,7 +31,8 @@ public class PlayerMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler( 0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f,targetAngle,0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            rb.velocity = moveDir.normalized * speed;  
+            //controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
 }
