45918e2470fffcd052d9ba9aff9bc822fc7e4a07
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2023-09-27 18:50:21 +0800

    Items-Added Paint and Item Script

--- Assets/Scripts/Paint.cs
+++ Assets/Scripts/Paint.cs
@@ -2,19 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Paint : Item
 {
-    private Material material;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private Material PaintColor;
 
 }
