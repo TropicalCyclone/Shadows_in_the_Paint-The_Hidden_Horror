1d0abbfa1534f58249a5d25663706d1ca50ff69f
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2023-09-29 16:40:39 +0800

    player_grab - debugged grab script

--- Assets/Scripts/PlayerGrab.cs
+++ Assets/Scripts/PlayerGrab.cs
@@ -18,49 +18,59 @@ public class PlayerGrab : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        
+     
         if (!Grabbed)
         {
             if (other.tag == "Item")
             {
+                Debug.Log("detected" + other);
                 if (keyDown)
                 {
+                    Debug.Log("grabbed" + other);
                     Rigidbody rb = other.GetComponent<Rigidbody>();
                     rb.isKinematic = true;
                     other.transform.parent = hand;
-                    other.transform.localPosition = Vector3.zero;
+                    while (other.transform.localPosition != Vector3.zero)
+                    {
+                        other.transform.localPosition = Vector3.Lerp(other.transform.localPosition, Vector3.zero, 2f);
+                    }
                     other.transform.localRotation = Quaternion.identity;
                     handObject = other.gameObject;
-                    if (hand.GetComponentInChildren<Rigidbody>().tag == "Item")
-                    {
-                        
-                        Grabbed = true;
-                    }
                 }
-
             }
         }
-        else
+
+       
+        /*else
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
                
             }
-        }
+        }*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        keyDown = Input.GetKey(KeyCode.E);
+        keyDown = Input.GetKeyDown(KeyCode.E);
 
-        
-        
+
+        if (hand.GetComponentInChildren<Rigidbody>() != null)
+        {
+            if (hand.GetComponentInChildren<Rigidbody>().tag == "Item")
+            {
+                Grabbed = true;
+                Debug.Log("grabbed");
+            }
+        }
+
         if (Grabbed && keyDown)
         {
             handObject.GetComponent<Rigidbody>().isKinematic = false;
             handObject.transform.parent = null;
-            Grabbed = false;
+            if (!hand.GetComponentInChildren<Rigidbody>())
+                Grabbed = false;
         }
     }
 }
