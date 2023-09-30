54d10bfbd0b49c3ac9390af07143ed4f799f5800
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2023-10-01 02:46:07 +0800

    Paint-Camo - Added Transparency When Hiding                        - modified Camo and Hide script

--- Assets/Scripts/Hide.cs
+++ Assets/Scripts/Hide.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Hide : MonoBehaviour
 {
     private MeshRenderer m_Renderer;
+    Material OriginalColor;
+    Color m_Color;
     // Start is called before the first frame update
     void Awake()
     {
@@ -15,7 +17,7 @@ public class Hide : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+         
     }
 
     private void OnTriggerEnter(Collider other)
@@ -23,19 +25,29 @@ public class Hide : MonoBehaviour
         //Debug.Log("found "+other.gameObject.tag);
         if (m_Renderer.sharedMaterial == other.GetComponent<MeshRenderer>().sharedMaterial && other.tag == "PaintWall"|| other.tag == "SafeZone")
         {
+            OriginalColor = m_Renderer.sharedMaterial;
+            m_Color = m_Renderer.material.color;
             Debug.Log("Player is Hiding");
             PlayerPrefs.SetInt("isHiding", 1);
+            m_Color.a = 0.5f;
+            m_Renderer.material.SetColor("_BaseColor", m_Color);
         }
        
     }
 
+    void setColor(Color inputColor)
+    {
+        m_Renderer.material.color = inputColor;
+    }
+
     private void OnTriggerExit(Collider other)
     {
-        MeshRenderer mat = other.GetComponent<MeshRenderer>();
-        if ((m_Renderer.sharedMaterial == other.GetComponent<MeshRenderer>().sharedMaterial && other.tag == "PaintWall") || other.tag == "SafeZone")
+        if ((OriginalColor == other.GetComponent<MeshRenderer>().sharedMaterial && other.tag == "PaintWall") || other.tag == "SafeZone")
         {
+            m_Color = m_Renderer.material.color;
             Debug.Log("Player is Visble");
             PlayerPrefs.SetInt("isHiding", 0);
+            m_Renderer.material = OriginalColor;
         }
     }
 }
