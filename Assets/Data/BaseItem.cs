5ac10358fa759789aa64aad898ff4b80f6145205
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2023-10-11 15:48:11 +0800

    Fix_Item_Heirarchy - Added Base item

---
+++ Assets/Data/BaseItem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseItem : MonoBehaviour
+{
+    [SerializeField]
+    private int id; 
+    [SerializeField]
+    private string itemName;
+    [SerializeField]
+    [TextArea]
+    private string description;
+    [SerializeField]
+    private Transform door;
+    [SerializeField]
+    private Sprite itemImage;
+}
