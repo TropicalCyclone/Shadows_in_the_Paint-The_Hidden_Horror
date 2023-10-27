097549a0b30653b2061ba76c3fcf782123c7e428
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2023-10-27 16:30:45 +0800

    Player-Interact-UI - modified UIManager, PlayerGrab, ItemManager, and DoorOpen scripts to utilize UI Interact

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _uiGroup;
+    [SerializeField] private TMP_Text _text; 
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
-        
+        _uiGroup.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SetText(string input)
+    {
+        _text.text = input;
+    }
+
+    public void SetUIVisual(bool value)
+    {
+        if (value)
+        {
+            _uiGroup.SetActive(true);
+        }
+        else
+        {
+            _uiGroup.SetActive(false);
+        }
     }
 }
