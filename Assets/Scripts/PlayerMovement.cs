dcb83c6f476b2aebfa72212cbcd0ba66769b1af7
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2023-11-02 16:30:54 +0800

    player-crouch - added crouch mechanic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,16 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Transform _cam;
     [SerializeField] private float _speed = 6f;
+    [SerializeField] private float _crouchSpeed = 3f;
     [SerializeField] private float _turnSmoothTime = 0.1f;
     [SerializeField] private float _turnSmoothVel;
-
+    private bool _isCrouching = false;
+    private float _walkingSpeed;
     private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
+        _walkingSpeed = _speed;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -21,6 +24,7 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+        bool playerCrouch = Input.GetButtonDown("Fire2");
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
@@ -33,6 +37,21 @@ public class PlayerMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f,targetAngle,0f) * Vector3.forward;
             rb.velocity = moveDir.normalized * _speed;  
             //controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        if (playerCrouch)
+        {
+            Debug.Log("button pressed");
+            _isCrouching = !_isCrouching;
+            if (!_isCrouching)
+            {
+                _speed = _walkingSpeed; 
+            }
+            else
+            {
+                _speed = _crouchSpeed;
+            }
+            Debug.Log(_speed);
         }
     }
 }
